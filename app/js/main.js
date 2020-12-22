2d49742a62da6aed7a1b968c73afd2f802a83fe8
iranavsegda <irsensimpson@gmail.com>
2020-12-22 16:01:56 +0200

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function(){
+
+	$('.count').each(function() {
+
+		$(this).prop('counter', 0).animate({
+	 
+		  counter: $(this).text()
+	 
+		}, {
+	 
+		  duration: 4000,
+	 
+		  easing: 'swing',
+	 
+		  step: function(now) {
+	 
+			 $(this).text(Math.ceil(now));
+		  }
+		});
+	 });
 	
 })
