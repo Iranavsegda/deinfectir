aa731d61c04ed30dd652e9de50d56fd876c34c62
iranavsegda <irsensimpson@gmail.com>
2020-12-30 17:22:46 +0200

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,17 @@
-$(function(){
+jQuery(function($){
 
-	$('.count').each(function() {
-
+	$('.count').each(function () {
 		$(this).prop('counter', 0).animate({
-	 
-		  counter: $(this).text()
-	 
+			counter: $(this).text()
 		}, {
-	 
-		  duration: 4000,
-	 
-		  easing: 'swing',
-	 
-		  step: function(now) {
-	 
-			 $(this).text(Math.ceil(now));
-		  }
+			duration: 4000,
+			easing: 'swing',
+			step: function (now) {
+				$(this).text(Math.ceil(now));
+			}
 		});
-	 });
-	
-})
+	});
+
+   $("#phone").mask("+38 (999) 999-99-99");
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,7 @@ function styles() {
 function scripts() {
 	return src([
 		'node_modules/jquery/dist/jquery.js',
+		'node_modules/jquery.maskedinput/src/jquery.maskedinput.js',
 		'app/js/main.js'
 	])
 	.pipe(concat('main.min.js'))
